de466e7d417ba91bb61cd2c8791d7beea29e51a6
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-04 01:35:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(arr)
+  arr.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(arr, idx1, idx2)
+  
+end
